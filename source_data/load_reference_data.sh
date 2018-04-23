ac5b7e816adde9eeacd66b8731af28573fb35611
Rob Gaston <rgaston@fargeo.com>
2018-04-23 08:21:09 -0700

    adds reference data and load script

---
+++ source_data/load_reference_data.sh
@@ -0,0 +1,8 @@
+createdb sltreference
+psql -U postgres -d sltreference -f setup_reference_db.sql
+psql -U postgres -d sltreference -f parcels_1.sql
+psql -U postgres -d sltreference -f parcels_2.sql
+psql -U postgres -d sltreference -f parcels_3.sql
+psql -U postgres -d sltreference -f parcels_4.sql
+psql -U postgres -d sltreference -f parcels_5.sql
+psql -U postgres -d sltreference -f streets.sql
