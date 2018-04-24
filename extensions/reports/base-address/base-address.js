3e40e4867c64170bfbc91bda59a3ad0c3f97c884
Rob Gaston <rgaston@fargeo.com>
2018-04-24 13:04:05 -0700

    highlights segment in base address report

--- extensions/reports/base-address/base-address.js
+++ extensions/reports/base-address/base-address.js
@@ -8,12 +8,15 @@ define(['knockout', 'viewmodels/report', 'views/components/widgets/map'], functi
             var tiles = this.report.get('tiles')
             this.feature_count = 0
             this.forReportManager = false
-
+            var segmentId;
             if (tiles) {
                 tiles.forEach(function(tile) {
                     _.each(tile.data, function(val, key) {
                         if (_.contains(val, 'FeatureCollection')) {
                             this.feature_count += 1
+                        }
+                        if (key === '1a08f80e-4746-11e8-b7cc-0242ac120006') {
+                            segmentId = parseInt(val);
                         }
                     }, this);
                 }, this)
@@ -22,7 +25,23 @@ define(['knockout', 'viewmodels/report', 'views/components/widgets/map'], functi
             }
             this.onInit = function (map) {
                 if (map) {
-                    console.log(map);
+                    map.on('load', function () {
+                        map.addLayer({
+                            "id": "highlighted-report-street",
+                            "type": "line",
+                            "source": "sf_streets",
+                            "source-layer": "sf_streets",
+                            "layout": {
+                                "line-join": "round",
+                                "line-cap": "round"
+                            },
+                            "paint": {
+                                "line-color": "#0029ff",
+                                "line-width": 8
+                            },
+                            "filter": ["==", "cnn", segmentId],
+                        });
+                    });
                 }
             }
         },
