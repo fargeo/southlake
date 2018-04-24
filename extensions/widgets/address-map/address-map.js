4ac6c8648a360b71e9bedab81cf596183bb93dc9
Rob Gaston <rgaston@fargeo.com>
2018-04-24 16:22:28 -0700

    adds streets to address map

--- extensions/widgets/address-map/address-map.js
+++ extensions/widgets/address-map/address-map.js
@@ -7,9 +7,45 @@ define([
             var self = this;
             params.onInit = function (map) {
                 if (map) {
-                    var segmentId = 657000;
                     self.map.on('load', function (map) {
-                        console.log(map);
+                        var streetsHighlightLayer = self.map.addLayer({
+                            "id": "highlighted-report-street",
+                            "type": "line",
+                            "source": "sf_streets",
+                            "source-layer": "sf_streets",
+                            "layout": {
+                                "line-join": "round",
+                                "line-cap": "round"
+                            },
+                            "paint": {
+                                "line-color": "#0029ff",
+                                "line-width": 8
+                            },
+                            "filter": ["==", "cnn", ""]
+                        });
+                        var parentTile = self.form.tiles["1a08d6c6-4746-11e8-b7cc-0242ac120006"];
+                        var segmentId;
+                        var findSegmentId = function () {
+                            var tiles = parentTile();
+                            if (tiles.length > 0) {
+                                segmentId = tiles[0].tiles["1a08e008-4746-11e8-b7cc-0242ac120006"]()[0].data['1a08f80e-4746-11e8-b7cc-0242ac120006'];
+                                if (ko.unwrap(segmentId)) {
+                                    updateFilter();
+                                }
+                                segmentId.subscribe(updateFilter);
+                            }
+                        };
+                        var updateFilter = function () {
+                            var id = ko.unwrap(segmentId);
+                            if (id) {
+                                id = parseInt(id);
+                            } else {
+                                id = "";
+                            }
+                            self.map.setFilter('highlighted-report-street', ["==", "cnn", id]);
+                        };
+                        findSegmentId();
+                        parentTile.subscribe(findSegmentId);
                     });
                 }
             }
